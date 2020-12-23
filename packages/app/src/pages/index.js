00dd8c7c5f7f5dc0ce3f9e1d7e574943bb62f9a0
hassaankhan235 <hassaankhan235@gmail.com>
2020-12-23 17:33:18 +0300

    theme added and netlify-identity-widget added

--- packages/app/src/pages/index.js
+++ packages/app/src/pages/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Container, Flex, Button, Heading } from 'theme-ui';
+import netlifyIdentity from 'netlify-identity-widget'
 
 export default props => {
+    useEffect(() => {
+        netlifyIdentity.init({})
+    })
     return(
-        <div>
-        Hello world
-        </div>
+        <Container bg='muted' sx={{paddingLeft:'100px',
+        paddingRight:'100px'}}>
+        <Flex sx={{flexDirection: 'column', padding:3}}>
+        <Heading>TODO APP </Heading>
+        <Button onClick={() => netlifyIdentity.open()}> Submit </Button>
+        </Flex>
+        </Container>
         )
 };
